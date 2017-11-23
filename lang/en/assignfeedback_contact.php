8f5adaf927f4631ab124b02dd9a07bb6cdb5aab6
Michael Hughes <michaelhughes@strath.ac.uk>
2017-11-23 13:40:29 +0000

    Langauge strings

---
+++ lang/en/assignfeedback_contact.php
@@ -0,0 +1,5 @@
+<?php
+
+$string['pluginname'] 		= 'Contact Students';
+$string['batchoperationconfirmcontactselected'] = 'Confirm contact selected students';
+$string['contactselected'] 	= 'Contact selected students';
